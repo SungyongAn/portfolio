2f151274bee3603d0ba10f43b8b1aee119cb9b0d
SUNGYONG AN <hanippemk2@gmail.com>
2024-11-20 11:31:48 +0900

    test

--- math_study_room/routes/calculation.py
+++ math_study_room/routes/calculation.py
@@ -41,7 +41,37 @@ def subtract(num_range, num_times):
     return question_list, answer
 
 
+# 割り算で使用する最大値
+def get_limit(num_range):
+    limit_zero = []
+    for _ in range(num_range):
+        limit_zero.append(9)
+
+    str_list = map(str, limit_zero)
+    limit = (int("".join(str_list)))
+    return limit
+
+
 # 割り算
+# def divide(num_range, num_times):
+#     limit = get_limit(num_range)
+#     question_list = []
+#     answer = []
+#     for i in range(num_times):
+#         if i == 0:
+#             question_list.append(random.randint(limit//2, limit))
+#         else:
+#             question_list.append(random.randint(2, limit//2))
+
+#     if question_list[0] % question_list[1] == 0:
+#         answer = question_list[0] // question_list[1]
+#     else:
+#         answer = str(question_list[0] // question_list[1]) + "と余り" + str(question_list[0] % question_list[1])
+#         for _ in range(2):
+#             answer.append(question_list[0] // question_list[1])
+#     return question_list, answer
+
+
 def divide(num_range):
     output_range = get_output_range(num_range)
     question_list = []
