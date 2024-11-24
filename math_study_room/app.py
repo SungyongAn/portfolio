7479da961328e857d25ebc46a6dac1c85197d622
SUNGYONG AN <hanippemk2@gmail.com>
2024-11-24 15:55:50 +0900

    add-flash mental arithmetic (tentative)

--- math_study_room/app.py
+++ math_study_room/app.py
@@ -1,6 +1,7 @@
 from fractions import Fraction
 import requests
 import streamlit as st
+import time
 # from PIL import Image
 
 
@@ -12,6 +13,12 @@ if 'residue' not in st.session_state:
     st.session_state.residues = []
 if 'show_answers' not in st.session_state:
     st.session_state.show_answers = False
+if 'flash_questions' not in st.session_state:
+    st.session_state.flash_questions = []
+if 'answers' not in st.session_state:
+    st.session_state.flash_answer = []
+if 'flash_show_answer' not in st.session_state:
+    st.session_state.show_flash_answer = False
 
 st.title('算数の勉強部屋')
 st.write('')
@@ -19,6 +26,7 @@ st.write('')
 st.sidebar.write("注意：半角小文字の数字を入力してください。")
 a = st.sidebar.text_input("問題数", value='5')
 b = st.sidebar.text_input("桁数", value='2')
+
 
 # 足し算
 if st.sidebar.button(" ＋ (足し算)"):
@@ -202,7 +210,53 @@ if divide:
                 st.json(response.json())
                 break
 
-# 各問題の出力
+
+# フラッシュ暗算
+if st.sidebar.button("フラッシュ暗算"):
+    try:
+        num_questions = int(a)
+        num_digits = int(b)
+    except ValueError:
+        st.sidebar.error("問題数と桁数には整数を入力してください。")
+    else:
+        st.session_state.flash_questions = []
+        st.session_state.start = ""
+        st.session_state.flash_show_answer = False
+
+    url = 'http://127.0.0.1:8000/page_add'
+    response = requests.post(url, json={"num_times": num_questions, "num_range": num_digits})
+    if response.status_code == 200:
+        data = response.json()
+        question_list = data["question_list"]
+        answer = int(data["answer"])
+        st.session_state.flash_questions = question_list
+        st.session_state.flash_answer = answer
+        screen = st.empty()
+        for i in range(int(a)):
+            if i == 0:
+                screen.markdown("<center>"'<p style="font-size: 50px;">START!</p>'"</center>", unsafe_allow_html=True)
+                time.sleep(1.5)
+                screen.markdown(" ")
+                time.sleep(0.5)
+                screen.markdown("<center>"f'<p style="font-size: 50px;">{question_list[i]}</p>'"</center>", unsafe_allow_html=True)
+            elif i == int(a) - 1:
+                screen.markdown("<center>"f'<p style="font-size: 50px;">{question_list[i]}</p>'"</center>", unsafe_allow_html=True)
+                time.sleep(1.5)
+                screen.markdown(" ")
+                time.sleep(0.5)
+                screen.markdown("<center>"'<p style="font-size: 60px;">finish!</p>'"</center>", unsafe_allow_html=True)
+            else:
+                screen.markdown("<center>"f'<p style="font-size: 60px;">{question_list[i]}</p>'"</center>", unsafe_allow_html=True)
+                time.sleep(1.5)
+                screen.markdown(" ")
+                time.sleep(0.5)
+    else:
+        st.error(f"{response.status_code}エラーが発生しました。詳細は以下を参照ください")
+        st.json(response.json())
+
+
+# 以下出力関係
+# 各計算問題の出力
 if st.session_state.questions:
     st.write("### 問題一覧")
     for idx, question in enumerate(st.session_state.questions, 1):
@@ -217,33 +271,10 @@ if st.session_state.questions:
             st.write(f'<p style="font-size: 20px;">問{idx}）{answer}</p>', unsafe_allow_html=True)
 
 
-# if st.sidebar.button("フラッシュ暗算"):
-#     url = 'http://127.0.0.1:8000/page_add'
-#     response = requests.post(url, json={"num_times": a, "num_range": b})
-#     st.write('')
-#     st.write('')
-#     if response.status_code == 200:
-#         multiply_file = response.json()
-#         question_list = multiply_file["question_list"]
-#         answer = multiply_file["answer"]
-        
-#         image_path = "/Users/sungyongan/Desktop/画像素材/corkboard.jpg"
-#         image = Image.open(image_path)
-#         st.image(image, use_column_width=True)
-            
-        # screen = st.empty()
-        # for i in range(int(a)):
-        #     if i == int(a) - 1:
-        #         screen.markdown("<center>"f'<p style="font-size: 50px;">{question_list[i]}</p>'"</center>", unsafe_allow_html=True)
-        #         time.sleep(1.5)
-        #         screen.markdown("<center>"'<p> </p>'"</center>")
-        #         time.sleep(0.5)
-        #         screen.markdown("<center>"'<p style="font-size: 60px;">finish!</p>'"</center>", unsafe_allow_html=True)
-        #     else:
-        #         screen.markdown("<center>"f'<p style="font-size: 60px;">{question_list[i]}</p>'"</center>", unsafe_allow_html=True)
-        #         time.sleep(1.5)
-        #         screen.markdown("<center>"'<p> </p>'"</center>")
-        #         time.sleep(0.5)
-                
-    # if right.button("正解"):
-    #     st.write(answer)
+# フラッシュ暗算の出力
+if st.session_state.flash_questions:
+    if st.button('正解表示'):
+        st.session_state.show_flash_answer = True
+        if st.session_state.show_flash_answer:
+            st.write("### 正解")
+            st.write(f'<p style="font-size: 20px;">{st.session_state.flash_answer}</p>', unsafe_allow_html=True)
