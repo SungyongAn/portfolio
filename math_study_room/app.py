7099a80c94f8d39d735f4d3ede80662b8832b8e1
SUNGYONG AN <hanippemk2@gmail.com>
2024-11-27 13:44:34 +0900

    del-flash mental arithmetic

--- math_study_room/app.py
+++ math_study_room/app.py
@@ -40,7 +40,7 @@ if st.sidebar.button(" ＋ (足し算)"):
         st.session_state.answers = []
         st.session_state.show_answers = False
 
-        for i in range(num_questions):
+        while len(st.session_state.questions) < num_questions:
             url = 'http://127.0.0.1:8000/page_add'
             response = requests.post(url, json={"num_times": 2, "num_range": num_digits})
             if response.status_code == 200:
@@ -48,8 +48,9 @@ if st.sidebar.button(" ＋ (足し算)"):
                 question_list = data["question_list"]
                 answer = data["answer"]
                 question = f"{question_list[0]} + {question_list[1]} = "
-                st.session_state.questions.append(question)
-                st.session_state.answers.append(int(answer))
+                if question not in st.session_state.questions:
+                    st.session_state.questions.append(question)
+                    st.session_state.answers.append(int(answer))
             else:
                 st.error(f"{response.status_code}エラーが発生しました。詳細は以下を参照ください")
                 st.json(response.json())
@@ -67,7 +68,7 @@ if st.sidebar.button(" ー (引き算)"):
         st.session_state.answers = []
         st.session_state.show_answers = False
 
-        for i in range(num_questions):
+        while len(st.session_state.questions) < num_questions:
             url = 'http://127.0.0.1:8000/page_subtract'
             response = requests.post(url, json={"num_times": 2, "num_range": num_digits})
             if response.status_code == 200:
@@ -75,8 +76,9 @@ if st.sidebar.button(" ー (引き算)"):
                 question_list = data["question_list"]
                 answer = data["answer"]
                 question = f"{question_list[0]} - {question_list[1]} ="
-                st.session_state.questions.append(question)
-                st.session_state.answers.append(int(answer))
+                if question not in st.session_state.questions:
+                    st.session_state.questions.append(question)
+                    st.session_state.answers.append(int(answer))
             else:
                 st.error(f"{response.status_code}エラーが発生しました。詳細は以下を参照ください")
                 st.json(response.json())
@@ -94,7 +96,7 @@ if st.sidebar.button(" × (掛け算)"):
         st.session_state.answers = []
         st.session_state.show_answers = False
 
-        for i in range(num_questions):
+        while len(st.session_state.questions) < num_questions:
             url = 'http://127.0.0.1:8000/page_multiply'
             response = requests.post(url, json={"num_times": 2, "num_range": num_digits})
             if response.status_code == 200:
@@ -102,16 +104,15 @@ if st.sidebar.button(" × (掛け算)"):
                 question_list = data["question_list"]
                 answer = data["answer"]
                 question = f"{question_list[0]} × {question_list[1]} ="
-                st.session_state.questions.append(question)
-                st.session_state.answers.append(int(answer))
+                if question not in st.session_state.questions:
+                    st.session_state.questions.append(question)
+                    st.session_state.answers.append(int(answer))
             else:
                 st.error(f"{response.status_code}エラーが発生しました。詳細は以下を参照ください")
                 st.json(response.json())
                 break
 
 # 割り算
-
-
 divide = st.sidebar.button(" ÷ (割り算)")
 
 kinds = st.sidebar.radio(
@@ -136,7 +137,7 @@ if divide:
             st.session_state.answers = []
             st.session_state.show_answers = False
 
-        for i in range(num_questions):
+        while len(st.session_state.questions) < num_questions:
             url = 'http://127.0.0.1:8000/page_divide'
             response = requests.post(url, json={"num_times": 2, "num_range": num_digits})
             if response.status_code == 200:
@@ -144,8 +145,9 @@ if divide:
                 question_list = data["question_list"]
                 answer = data["answer"]
                 question = f"{question_list[0]} ÷ {question_list[1]} ="
-                st.session_state.questions.append(question)
-                st.session_state.answers.append(answer)
+                if question not in st.session_state.questions:
+                    st.session_state.questions.append(question)
+                    st.session_state.answers.append(int(answer))
             else:
                 st.error(f"{response.status_code}エラーが発生しました。詳細は以下を参照ください")
                 st.json(response.json())
@@ -211,61 +213,19 @@ if divide:
                 break
 
 
-# フラッシュ暗算
-if st.sidebar.button("フラッシュ暗算"):
-    try:
-        num_questions = int(a)
-        num_digits = int(b)
-    except ValueError:
-        st.sidebar.error("問題数と桁数には整数を入力してください。")
-    else:
-        st.session_state.flash_questions = []
-        st.session_state.start = ""
-        st.session_state.flash_show_answer = False
-
-    url = 'http://127.0.0.1:8000/page_add'
-    response = requests.post(url, json={"num_times": num_questions, "num_range": num_digits})
-    if response.status_code == 200:
-        data = response.json()
-        question_list = data["question_list"]
-        answer = int(data["answer"])
-        st.session_state.flash_questions = question_list
-        st.session_state.flash_answer = answer
-        screen = st.empty()
-        for i in range(int(a)):
-            if i == 0:
-                screen.markdown("<center>"'<p style="font-size: 50px;">START!</p>'"</center>", unsafe_allow_html=True)
-                time.sleep(1.5)
-                screen.markdown(" ")
-                time.sleep(0.5)
-                screen.markdown("<center>"f'<p style="font-size: 50px;">{question_list[i]}</p>'"</center>", unsafe_allow_html=True)
-                time.sleep(1.5)
-                screen.markdown(" ")
-                time.sleep(0.5)
-            elif i == int(a) - 1:
-                screen.markdown("<center>"f'<p style="font-size: 50px;">{question_list[i]}</p>'"</center>", unsafe_allow_html=True)
-                time.sleep(1.5)
-                screen.markdown(" ")
-                time.sleep(0.5)
-                screen.markdown("<center>"'<p style="font-size: 60px;">finish!</p>'"</center>", unsafe_allow_html=True)
-            else:
-                screen.markdown("<center>"f'<p style="font-size: 60px;">{question_list[i]}</p>'"</center>", unsafe_allow_html=True)
-                time.sleep(1.5)
-                screen.markdown(" ")
-                time.sleep(0.5)
-    else:
-        st.error(f"{response.status_code}エラーが発生しました。詳細は以下を参照ください")
-        st.json(response.json())
-
-
-# 以下出力関係
 # 各計算問題の出力
 if st.session_state.questions:
     answer_list = []
     st.write("### 問題一覧")
+    # for idx, question in enumerate(st.session_state.questions, 1):
+    #     st.write(f'<p style="font-size: 20px;">問{idx}) {question} </p>', unsafe_allow_html=True)
+    #     answer_list.append(st.text_input(f"回答{idx}", value='0'))
     for idx, question in enumerate(st.session_state.questions, 1):
-        st.write(f'<p style="font-size: 20px;">問{idx}){question}</p>', unsafe_allow_html=True)
-        answer_list.append(st.text_input(f"回答{idx}"))
+        output = st.empty()
+        output.write(f'<p style="font-size: 20px;">問{idx}) {question}</p>', unsafe_allow_html=True)
+        ans = st.text_input(f"回答{idx}", value='0')
+        answer_list.append(ans)
+        output.write(f'<p style="font-size: 20px;">問{idx}) {question} {ans}</p>', unsafe_allow_html=True)
 
     if st.button('正解表示'):
         st.session_state.show_answers = True
@@ -279,17 +239,4 @@ if st.session_state.questions:
                 st.write("○")
             else:
                 st.write("×")
-                
 
-
-# フラッシュ暗算の出力
-if st.session_state.flash_questions:
-    answer = st.text_input("回答")
-    if st.button('正解表示'):
-        st.session_state.show_flash_answer = True
-        if st.session_state.show_flash_answer:
-            st.write(f'<p style="font-size: 20px;">{st.session_state.flash_answer}</p>', unsafe_allow_html=True)
-            if st.session_state.flash_answer == int(answer):
-                st.write("○")
-            else:
-                st.write("×")
