7a4830df79a84d30d2febbf1f8dc6e841e9bcfb2
SUNGYONG AN <hanippemk2@gmail.com>
2024-12-05 11:10:16 +0900

    please check the contents for details as there are many changes

--- math_study_room/routes/api.py
+++ math_study_room/routes/api.py
@@ -1,39 +1,46 @@
 from fastapi import APIRouter
 from routes.schema import (
     CaluculationPayload,
+    DividePayload,
     CaluculationResponse,
     Divide_residueResponse,
 )
-from routes.calculation import add, subtract, divide, divide_residue, multiply
+from routes.calculation import addition, subtract, divide, divide_residue, multiply
 
 router = APIRouter()
 
 
-@router.post("/page_add")
-async def page_add(caluculation_payload:CaluculationPayload) -> CaluculationResponse:
-    question_list, answer = add(caluculation_payload.num_times, caluculation_payload.num_range)
+@router.post("/page_addition")
+async def page_addition(caluculation_payload:CaluculationPayload) -> CaluculationResponse:
+    question_list, answer = addition(
+        caluculation_payload.num_range,
+        caluculation_payload.identification_code
+        )
     return CaluculationResponse(question_list=question_list, answer=answer)
 
 
 @router.post("/page_subtract")
 async def page_subtract(caluculation_payload:CaluculationPayload) -> CaluculationResponse:
-    question_list, answer = subtract(caluculation_payload.num_times, caluculation_payload.num_range)
+    question_list, answer = subtract(
+        caluculation_payload.num_range,
+        caluculation_payload.identification_code
+        )
     return CaluculationResponse(question_list=question_list, answer=answer)
 
 
 @router.post("/page_divide")
-async def page_divide(caluculation_payload:CaluculationPayload) -> CaluculationResponse:
-    question_list, answer = divide(caluculation_payload.num_times, caluculation_payload.num_range)
+async def page_divide(dividePayload:DividePayload) -> CaluculationResponse:
+    question_list, answer = divide(dividePayload.num_range)
     return CaluculationResponse(question_list=question_list, answer=answer)
 
 
 @router.post("/page_divide_residue")
-async def page_divide_residue(caluculation_payload:CaluculationPayload) -> Divide_residueResponse:
-    question_list, answer, residue = divide_residue(caluculation_payload.num_times, caluculation_payload.num_range)
+async def page_divide_residue(dividePayload:DividePayload) -> Divide_residueResponse:
+    question_list, answer, residue = divide_residue(dividePayload.num_range)
     return Divide_residueResponse(question_list=question_list, answer=answer, residue=residue)
 
 
 @router.post("/page_multiply")
 async def page_multiply(caluculation_payload:CaluculationPayload) -> CaluculationResponse:
-    question_list, answer = multiply(caluculation_payload.num_times, caluculation_payload.num_range)
+    question_list, answer = multiply(caluculation_payload.num_range, caluculation_payload.identification_code)
     return CaluculationResponse(question_list=question_list, answer=answer)
--- math_study_room/routes/test.py
+++ math_study_room/routes/test.py
@@ -1,6 +1,7 @@
 import random
 
 
+# 整数）出題される数字の最小値、最大値を作成
 def get_output_range(num_range):
     output_range = []
     for i in range(2):
@@ -14,19 +15,40 @@ def get_output_range(num_range):
         else:
             for j in range(num_range):
                 output_range_zero.append(9)
-        str_list = map(str,output_range_zero)
+        str_list = map(str, output_range_zero)
         output_range.append(int("".join(str_list)))
     return output_range
 
 
-def multiply(num_range, num_times):
-    output_range = get_output_range(num_range)
-    question_list = [random.randint(output_range[0], output_range[1]) for _ in range(num_times)]
-    answer = question_list[0] * question_list[1]
-    print(question_list)
+def get_range_real_numbers(num_range):
+    output_range = []
+    for i in range(2):
+        if i == 0:
+            output_range.append(1)
+        else:
+            output_range_zero = []
+            for j in range(num_range + 1):
+                if j == 0:
+                    output_range_zero.append(1)
+                else:
+                    output_range_zero.append(9)
+            str_list = map(str, output_range_zero)
+            output_range.append(int("".join(str_list)))
+    return output_range
+
+
+# 掛け算
+def multiply(num_times, num_range, type):
+    if type == 1:
+        output_range = get_output_range(num_range)
+        question_list = [random.randint(output_range[0], output_range[1]) for _ in range(num_times)]
+        answer = question_list[0] * question_list[1]
+    elif type == 2:
+        output_range = get_range_real_numbers(num_range)
+        question_list_zero = [random.randint(output_range[0], output_range[1]) for _ in range(num_times)]
+        idenominator = 10 ** (num_range)
+        question_list = [question_list_zero[0] / idenominator, question_list_zero[1] / idenominator]
+        answer = question_list_zero[0] * question_list_zero[1] / (idenominator * idenominator)
     return question_list, answer
 
-num_range = 3
-num_times = 2
-multiply(num_range, num_times)
-
+multiply(2, 3, 2)
