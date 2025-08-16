dc5c0b261fe815c0f59eb233031a9dac8675c40c
SUNGYONG AN <hanippemk2@gmail.com>
2025-08-16 20:07:38 +0900

    参考書75Pまで学習

--- study_javascript/ToDo_vue/app.js
+++ study_javascript/ToDo_vue/app.js
@@ -8,6 +8,33 @@ Vue.createApp({
             hideDoneToDo: false,
             searchWord: "",
             order: "desc",
+            categoryName: "",
         }
+    },
+    computed: {
+        canCreateToDo: function() {
+            return this.todoTitle !==""
+        },
+        canCreateCategory: function () {
+            return this.categoryName !== ""
+        }
+    },
+    methods: {
+        createTodo: function () {
+            if (!this.canCreateToDo) {
+                return
+            }
+
+            // トゥドゥタスクを追加する処理
+            this.todoTitle = ""
+            this.todoDescription = ""
+            this.todoCategories = []
+        },
+    createCategory: function() {
+        if (!this.canCreateCategory) {
+                return
+            }
+            this.categoryName = ""
+        },
     },
 }).mount("#app")
