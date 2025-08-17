07bad04d44ecd83ecb0beb4cb6c6e2018147432e
SUNGYONG AN <hanippemk2@gmail.com>
2025-08-17 16:44:06 +0900

    86Pまで学習

--- study_javascript/ToDo_vue/app.js
+++ study_javascript/ToDo_vue/app.js
@@ -4,7 +4,9 @@ Vue.createApp({
             todoTitle: "",
             todoDescription: "",
             todoCategories: [],
-            selectCategoly: "",
+            selectCategory: "",
+            todos: [],
+            categories: [],
             hideDoneToDo: false,
             searchWord: "",
             order: "desc",
@@ -12,29 +14,78 @@ Vue.createApp({
         }
     },
     computed: {
+        // タイトル未入力は除外
         canCreateToDo: function() {
-            return this.todoTitle !==""
+            return this.todoTitle !== ""
         },
-        canCreateCategory: function () {
-            return this.categoryName !== ""
+        // カテゴリー未入力、入力したカテゴリーの有無を確認
+        canCreateCategory: function() {
+            return this.categoryName !== "" && !this.existsCategory
+        },
+        existsCategory: function() {
+            const categoryName = this.categoryName
+            // ) !== -1 は見つけた場合はTrue、見つからない場合はFalseを返す。
+            // 下記の内容だと categories に categoryName が存在するか確認
+            return this.categories.indexOf(categoryName) !== -1
         }
     },
+
+    watch: {
+        todos: {
+            handler: function(next) {
+                window.localStorage.setItem("todos", JSON.stringify(next))
+            },
+            deep: true,
+        },
+        categories: {
+            handler: function(next) {
+                window.localStorage.setItem("categories", JSON.stringify(next))
+            },
+            deep: true,
+        },
+    },
+
     methods: {
         createTodo: function () {
             if (!this.canCreateToDo) {
                 return
             }
+            this.todos.push({
+                id: "todo-" + Date.now(),
+                title: this.todoTitle,
+                description: this.todoDescription,
+                categories: this.todoCategories,
+                dateTime: Date.now(),
+                done: false,
+            })
 
             // トゥドゥタスクを追加する処理
             this.todoTitle = ""
             this.todoDescription = ""
             this.todoCategories = []
         },
-    createCategory: function() {
-        if (!this.canCreateCategory) {
-                return
-            }
-            this.categoryName = ""
+
+        createCategory: function() {
+            if (!this.canCreateCategory) {
+                    return
+                }
+
+                this.categories.push(this.categoryName)
+
+                this.categoryName = ""
         },
+    },
+
+    created: function() {
+        const todos = window.localStorage.getItem("todos")
+        const categories = window.localStorage.getItem("categories")
+
+        if (todos) {
+            this.todos = JSON.parse(todos)
+        }
+
+        if (categories) {
+            this.categories = JSON.parse(categories)
+        }
     },
 }).mount("#app")
